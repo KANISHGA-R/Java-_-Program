2c93763b0eb69795b9b615f65672b9484d777467
KANISHGA-R <kanishkarajkumar05@gmail.com>
2026-05-02 14:45:50 +0530

    To find the length
    Signed-off-by: KANISHGA-R <kanishkarajkumar05@gmail.com>

---
+++ length1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class length1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        if(text.length() < 5 ){
+            System.out.println("Short");
+        }
+        else{
+            System.out.println("Long");
+        }
+    }
+}
