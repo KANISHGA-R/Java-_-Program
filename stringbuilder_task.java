40da7e514e4762bb4f9a012ce43288d991f75123
KANISHGA-R <kanishkarajkumar05@gmail.com>
2026-05-04 09:37:15 +0530

    StringBuilder
    Signed-off-by: KANISHGA-R <kanishkarajkumar05@gmail.com>

---
+++ stringbuilder_task.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class stringbuilder_task{
+    public static void main(String[] args){
+    StringBuilder s = new StringBuilder("Arun From Chennai Loves Cricket");
+   
+    String upper = s.toString().toUpperCase();
+    System.out.println(upper);
+    }
+}
