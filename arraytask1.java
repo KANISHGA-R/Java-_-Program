d7f6f2eeade5835f997717a549eca883db316027
KANISHGA-R <kanishkarajkumar05@gmail.com>
2026-05-05 14:35:23 +0530

    Creating an empty array , adding elements to the array and traversing the array
    Signed-off-by: KANISHGA-R <kanishkarajkumar05@gmail.com>

---
+++ arraytask1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class arraytask1{
+    public static void main(String[] args){
+        int arr[] = new int[5];
+        arr[0]=100;
+        arr[1]=200;
+        arr[2]=300;
+        arr[3]=400;
+        arr[4]=500;
+       
+        System.out.println("the array values are: " +Arrays.toString(arr));
+        arr[4]=5000;
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
