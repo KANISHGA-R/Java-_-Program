bdb9fbbf1800cc64deb2b49a57da254abf07b906
KANISHGA-R <kanishkarajkumar05@gmail.com>
2026-05-05 14:39:16 +0530

    Creating array with set of values then getting input from the user and comparing the users input with the set of values in the array
    Signed-off-by: KANISHGA-R <kanishkarajkumar05@gmail.com>

---
+++ arraytask2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class arraytask2{
+    public static void main(String[] args){
+        int[] arr={20,40,50,80,88};
+        Scanner sc=new Scanner(System.in);
+        //System.out.println("the array: " + Arrays.toString(arr));
+        if (sc==arr()){
+            System.out.println("ID found");
+        }
+        else{
+            System.out.println("Not found");
+        }
+
+
+    }
+}
